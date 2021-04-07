07282db0b55c7c9e6432a4ecaf772cb66a1eb626
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2021-04-07 17:17:04 +0300

    + test commit (dump) for the test view_rotate_sycl

--- test/parallel_api/ranges/rotate_view_sycl.pass.cpp
+++ test/parallel_api/ranges/rotate_view_sycl.pass.cpp
@@ -39,7 +39,7 @@ main()
 
     auto view = nano::ranges::views::all(data) | ranges::views::rotate(rotate_val);
     {
-        sycl::buffer<int> A(expected, sycl::range<1>(max_n));
+        sycl::buffer<int> A(expected, expected + max_n);
         A.set_final_data(expected);
         A.set_write_back(true);
 
@@ -49,6 +49,18 @@ main()
     //check aasigment
     view[4] = -1;
     expected[4] = -1;
+
+    ::std::cout << "view: ";
+    for(auto v: view)
+        ::std::cout << v << "  ";
+    ::std::cout << ::std::endl;
+
+    ::std::cout << "expected: ";
+    for(auto v: expected)
+        ::std::cout << v << "  ";
+    ::std::cout << ::std::endl;
+
+    ::std::cout << view.end() - view.begin() << ::std::endl;
 
     EXPECT_EQ_N(view.begin(), expected, max_n, "wrong result from rotate view on a device");
 
